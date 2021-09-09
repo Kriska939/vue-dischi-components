ef9ae0f72f354c9dc7dcf47f913a3f078a4234c8
Kriska939 <cristinacorrado93@gmail.com>
2021-09-09 17:00:07 +0200

    added album + album cards components

---
+++ src/components/Albums.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <section id="album-container">
+      <div class="card" v-for="(album, index) in albumList" :key="index">
+        <Cards :album="album" />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Cards from "./Cards.vue";
+export default {
+  data() {
+    return {
+      albumList: [],
+    };
+  },
+  name: "albums",
+  components: {
+    Cards,
+  },
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((r) => {
+        this.albumList = r.data.response;
+        console.log(this.albumList);
+      });
+  },
+};
+</script>
+
+<style scoped lang="scss">
+#album-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.card {
+  background-color: #2e3a46;
+  padding: 20px;
+  margin: 20px;
+  flex-basis: 200px;
+  text-align: center;
+  min-height: 350px;
+}
+</style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <img :src="album.poster" :alt="album.title" />
+    <h2 class="title">{{ album.title }}</h2>
+    <address class="author">{{ album.author }}</address>
+    <time :datetime="album.year" class="year">{{ album.year }}</time>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["album"],
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/scss/_vars.scss";
+
+img {
+  width: 100%;
+  height: auto;
+}
+
+.title {
+  color: #fff;
+  margin: 15px 0;
+  font-size: 20px;
+}
+
+.author,
+.year {
+  color: $color-grey;
+}
+
+.author {
+  font-size: 16px;
+}
+.year {
+  font-size: 14px;
+}
+</style>
